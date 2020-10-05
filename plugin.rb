87c0c60e6fe81c6a16ae5f65fd13a180a2dc9c3a
Shivam Khandelwal <skbly7@gmail.com>
2020-10-05 16:00:27 +0530

    Fixes for enabling colab rendering

--- plugin.rb
+++ plugin.rb
@@ -7,12 +7,11 @@
 class Onebox::Engine::IpynbOnebox
     include Onebox::Engine
 
+    REGEX = /(^(https?:)?\/\/.*\.ipynb(\?.*)?$)|(^(https?:)?\/\/colab.research.google.*$)/i
+    matches_regexp REGEX
+
     IPYNB_REGEX = /^(https?:)?\/\/.*\.ipynb(\?.*)?$/i
     COLAB_REGEX = /^(https?:)?\/\/colab.research.google.*$/i
-    if matches_regexp IPYNB_REGEX
-        url_to_fetch = "http://#{ipynb_render_service}/render/ipynb?url=#{@url}&hash=#{auth_hash}"
-    elsif matches_regexp COLAB_REGEX
-        url_to_fetch = "http://#{ipynb_render_service}/render/colab?url=#{@url}&hash=#{auth_hash}"
 
     always_https
 
@@ -20,6 +19,13 @@ class Onebox::Engine::IpynbOnebox
         ipynb_render_service = ENV['ONEBOX_IPYNB_RENDERER_HOSTNAME']
         secret = ENV['ONEBOX_IPYNB_RENDERER_SECRET']
         auth_hash = Digest::MD5.hexdigest(secret + @url)
+
+        if ( @url =~ IPYNB_REGEX )
+            url_to_fetch = "http://#{ipynb_render_service}/render/ipynb?url=#{@url}&hash=#{auth_hash}"
+        elsif ( @url =~ COLAB_REGEX )
+            url_to_fetch = "http://#{ipynb_render_service}/render/colab?url=#{@url}&hash=#{auth_hash}"
+        end
+
         @raw ||= Onebox::Helpers.fetch_html_doc(url_to_fetch)
 
         <<-HTML
